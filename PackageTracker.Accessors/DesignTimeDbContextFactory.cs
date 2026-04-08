760df35fb5b7b8aa9ce14e72e534113020ea7c61
asmith183 <asmith183@unl.edu>
2026-04-08 16:12:57 -0500

    Deleted redundant DbContext, updated DbContextFactory to use correct DbContext

--- PackageTracker.Accessors/DesignTimeDbContextFactory.cs
+++ PackageTracker.Accessors/DesignTimeDbContextFactory.cs
@@ -1,27 +1,26 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
-using System.IO;
+using PackageTracker.Accessors.Data;
 
-namespace PackageTracker.Accessors
+namespace PackageTracker.Accessors;
+// EF tools will use this to create the DbContext when running migrations from the CLI
+public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
 {
-    // EF tools will use this to create the DbContext when running migrations from the CLI
-    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<PackageTrackerDbContext>
+    public AppDbContext CreateDbContext(string[] args)
     {
-        public PackageTrackerDbContext CreateDbContext(string[] args)
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: true);
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true);
 
-            var config = builder.Build();
-            var conn = config.GetConnectionString("DefaultConnection") ??
-                       "Server=(localdb)\\mssqllocaldb;Database=PackageTrackerDb;Trusted_Connection=True;MultipleActiveResultSets=true";
+        var config = builder.Build();
+        var conn = config.GetConnectionString("DefaultConnection") ??
+                "Server=localhost,1433;Database=PackageTrackerDB;User Id=sa;Password=changeme;TrustServerCertificate=True;";
 
-            var optionsBuilder = new DbContextOptionsBuilder<PackageTrackerDbContext>();
-            optionsBuilder.UseSqlServer(conn);
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+        optionsBuilder.UseSqlServer(conn);
 
-            return new PackageTrackerDbContext(optionsBuilder.Options);
-        }
+        return new AppDbContext(optionsBuilder.Options);
     }
 }
+
